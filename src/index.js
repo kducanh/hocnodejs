3a9c07b2e221845fe611c317ed7e111e0ac5f600
kducanh <k.ducanh1994@gmail.com>
2018-11-15 15:13:47 +0700

    Update ngay 15/11

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
+const userRouter = require("./router");
+
 const app = express();
 const port = 3000;
 
-app.get("/",(req,res)=>res.send("Hello World !!!!!"));
+app.get("/", (req,res) => res.send("/n Hello World GET !!!!!"));
+app.use("/users/",userRouter);
+
+
 app.listen(port,()=>
     console.log(`Server running at http://127.0.0.1:${port}/`)
 );
